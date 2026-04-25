b82dfe9181e93ed21ce1fec6d580f252b3e125c6
chris <chris.nikiforou@gmail.com>
2026-04-25 14:41:30 -0400

    db full table creation

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,10 +6,12 @@ namespace CoffeeShopMVC.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) {}
 
         public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
     }
 }
---
+++ Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeShopMVC.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; } // PK
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
