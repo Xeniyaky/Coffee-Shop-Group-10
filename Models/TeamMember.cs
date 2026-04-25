e781c0b3c3ad33b349f5450631ba49d024a64a54
chris <chris.nikiforou@gmail.com>
2026-04-25 13:14:44 -0400

    Added sqlite for dev purposes and created first table

---
+++ Models/TeamMember.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopMVC.Models
+{
+    public class TeamMember
+    {
+        public int Id { get; set; } // Primary Key
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Description { get; set; }
+    }
+}
