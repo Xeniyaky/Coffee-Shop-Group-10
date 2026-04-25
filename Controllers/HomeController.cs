807f893a78102e33ef500a69943d51ea19692fef
Xeniya <xeniyak@usf.edu>
2026-04-25 13:46:20 -0400

    Added working nutrition API and finalized UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using CoffeeShopMVC.Models;
 
 namespace CoffeeShopMVC.Controllers
 {
@@ -18,6 +20,49 @@ namespace CoffeeShopMVC.Controllers
         public IActionResult Charts()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Nutrition(string query)
+        {
+            var nutritionItems = new List<NutritionItem>();
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                var apiKey = "qJjwWaC9tV7wGlMrr9HkQYyiYeFmD4YXlYWDOVlb";
+                var url = $"https://api.api-ninjas.com/v1/nutrition?query={Uri.EscapeDataString(query)}";
+
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
+
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode && json.Trim().StartsWith("["))
+                {
+                    using var document = JsonDocument.Parse(json);
+
+                    foreach (var item in document.RootElement.EnumerateArray())
+                    {
+                        nutritionItems.Add(new NutritionItem
+                        {
+                            Name = item.GetProperty("name").ToString(),
+                            Calories = item.GetProperty("calories").ToString(),
+                            ServingSizeG = item.GetProperty("serving_size_g").ToString(),
+                            FatTotalG = item.GetProperty("fat_total_g").ToString(),
+                            ProteinG = item.GetProperty("protein_g").ToString(),
+                            CarbohydratesTotalG = item.GetProperty("carbohydrates_total_g").ToString(),
+                            SugarG = item.GetProperty("sugar_g").ToString()
+                        });
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = json;
+                }
+            }
+
+            ViewBag.Query = query;
+            return View(nutritionItems);
         }
 
         public IActionResult Privacy()
