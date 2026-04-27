b4844084f153665e1e4dcad9b188f4cb6a23f05e
Xeniya <xeniyak@usf.edu>
2026-04-27 19:45:39 -0400

    Added ERD image to README

--- obj/Debug/net10.0/CoffeeShopMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/CoffeeShopMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShopMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafc4cbf6141d116b7e7dbbcbbc0223ca623e112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd591da30667da43c29ae570390be05726e349ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShopMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShopMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
